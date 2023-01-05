3e6029d7537b1b5221d09c9fd400a09403695b12
ClearScript <ClearScript@microsoft.com>
2023-01-04 21:24:09 -0500

    Fixed underscore test; ES modules need to import underscore-esm.js.

--- test_modules_function_clearscript/Scripts/index.js
+++ test_modules_function_clearscript/Scripts/index.js
@@ -4,6 +4,6 @@
 //Console.AppendText(JSON.stringify(data));
 
 //Example with underscore
-import * as _ from 'underscore'
+import * as _ from 'underscore-esm'
 var data = _.map([1, 2, 3], function (num) { return num * 3; });
 Console.AppendText(JSON.stringify(data));
